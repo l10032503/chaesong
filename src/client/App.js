4f8c81288db89109eccd7071ce6ebbdfd0169a54
l100320503 <l10032503@gmail.com>
2019-03-26 16:46:39 +0900

    db 연결

--- src/client/App.js
+++ src/client/App.js
@@ -11,7 +11,7 @@ class MyComponent extends Component {
             <div>
                 <h1>{this.state.hi}</h1>
                 <h1>chaesong react express test</h1>
-                <h1>update test</h1>
+                <h1>build 후 확인</h1>
             </div>
         );
     }
--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,48 @@
+const mysql = require("mysql");
 const express = require("express");
-
 const app = express();
 
 app.use(express.static("dist"));
 
-app.get("/api/getUsername", (req, res) =>
-    res.send("Hi!")
+const connection  = mysql.createConnection({
+    "host"         : "chaesong.cccteklwfdo9.ap-northeast-2.rds.amazonaws.com",
+    "user"         : "comhong",
+    "password"      : "sook2019",
+    "database"      : "chaesongdb"
+});
+
+connection.connect();
+
+app.get("/show", (req, res) =>
+    connection.query("SELECT * FROM test", (err, rows) => {
+        if(err) throw err;
+        res.send(rows);
+    })
 );
-app.listen(process.env.PORT || 2019, () => console.log("Listening on port 2019!"));
+app.post('/register', function(req, res){
+    var user_id = req.body.user_id;
+    var pw = req.body.pw;
+    var birthyear = req.body.birthyear;
+    var height = req.body.height;
+    var weight = req.body.weight;
+    var active = req.body.active;
+    var register_date = req.body.register;
+    var vegantype = req.body.vegantype;
+
+    var sql = 'insert into MemberJoin (content) VALUE(?)';
+    var params = [content];
+    connection.query(sql, params, function(error, results, fields){
+        if(error){
+            console.log(error);
+            res.json({message: 'fail'});
+        }
+        else{
+            console.log('success!');
+            res.json({message: 'complete'});
+        }
+    });
+});
+
+app.listen(4000,function(){
+    console.log("Started listening on port", 4000);
+});
